df76a5dde2232f973102a375a070794562da10aa
Mel <8953419+wabbitguy@users.noreply.github.com>
2026-03-30 21:21:18 -0700

    Better layout, icons, end time

--- Settings.h
+++ Settings.h
@@ -42,8 +42,10 @@ SOFTWARE.
 // maximums for bed temp and nozzle temp
 uint8_t maxBedTemp = 120;
 uint16_t maxNozzleTemp = 350;
+
 //this the IP information of your printer on the LAN
 String printerIP = "192.168.1.6";  // just a default IP address, put yours in if you know it
+
 String printQuery = ":7125/printer/objects/query?print_stats&display_status&extruder&heater_bed";
 String printerINFO = ":7125/printer/info";
 String printerURLQ = "";
@@ -51,7 +53,9 @@ String printerURLInfo = "";
 String thePrintFile = "";  // filename of the currently printing file
 
 // This is the graphic that shows when the printer is powered off
-#define OFFLINE_IMAGE "/Asleep.bmp"  // graphics MUST be in 24bit format 186 x 186 (H x W)
+#define OFFLINE_IMAGE "/Asleep.bmp"   // graphics MUST be in 24bit format 186 x 186 (H x W)
+#define IDLE_IMAGE "/Idle.bmp"        // graphic when active but not printing (64 x 64)
+#define HEATING_IMAGE "/Heating.bmp"  // graphic to indicate heating
 //
 // ---------------- SD Card FILES ----------------
 #define SD_MOSI 23  // pin numbers for the CDY 2.8 7789
@@ -59,13 +63,56 @@ String thePrintFile = "";  // filename of the currently printing file
 #define SD_SCK 18
 #define SD_CS 5
 
-// ---------------- TIME HELPERS ----------------
-//Edit These Lines According To Your Timezone and Daylight Saving Time
+//Australia Eastern Time Zone (Sydney, Melbourne)
+TimeChangeRule aEDT = { "AEDT", First, Sun, Oct, 2, 660 };  //UTC + 11 hours
+TimeChangeRule aEST = { "AEST", First, Sun, Apr, 3, 600 };  //UTC + 10 hours
+//Timezone timeZoneRule(aEDT, aEST);
+
+//Central European Time (Frankfurt, Paris)
+TimeChangeRule CEST = { "CEST", Last, Sun, Mar, 2, 120 };  //Central European Summer Time
+TimeChangeRule CET = { "CET ", Last, Sun, Oct, 3, 60 };    //Central European Standard Time
+//Timezone timeZoneRule(CEST, CET);
+
+//United Kingdom (London, Belfast)
+TimeChangeRule BST = { "BST", Last, Sun, Mar, 1, 60 };  //British Summer Time
+TimeChangeRule GMT = { "GMT", Last, Sun, Oct, 2, 0 };   //Standard Time
+//Timezone timeZoneRule(BST, GMT);
+
+//US Eastern Time Zone (New York, Detroit)
+TimeChangeRule usEDT = { "EDT", Second, Sun, Mar, 2, -240 };  //Eastern Daylight Time = UTC - 4 hours
+TimeChangeRule usEST = { "EST", First, Sun, Nov, 2, -300 };   //Eastern Standard Time = UTC - 5 hours
+//Timezone timeZoneRule(usEDT, usEST);
+
+//US Central Time Zone (Chicago, Houston)
+TimeChangeRule usCDT = { "CDT", Second, dowSunday, Mar, 2, -300 };
+TimeChangeRule usCST = { "CST", First, dowSunday, Nov, 2, -360 };
+//Timezone timeZoneRule(usCDT, usCST);
+
+//US Mountain Time Zone (Denver, Salt Lake City)
+TimeChangeRule usMDT = { "MDT", Second, dowSunday, Mar, 2, -360 };
+TimeChangeRule usMST = { "MST", First, dowSunday, Nov, 2, -420 };
+//Timezone timeZoneRule(usMDT, usMST);
+
+//Arizona is US Mountain Time Zone but does not use DST
+//Timezone timeZoneRule(usMST, usMST);
+
+//US Pacific Time Zone (Las Vegas, Los Angeles)
+TimeChangeRule usPDT = { "PDT", Second, dowSunday, Mar, 2, -420 };
+TimeChangeRule usPST = { "PST", First, dowSunday, Nov, 2, -480 };
+//Timezone timeZoneRule(usPDT, usPST);
+//
+// ---------------- TIME HELPER ----------------
 //TimeZone Settings Details https://github.com/JChristensen/Timezone
-//US Pacific Time Zone
-TimeChangeRule bcPDT = { "PDT", Second, dowSunday, Mar, 2, -420 };  // 7 hour offset
-TimeChangeRule usPST = { "PST", First, dowSunday, Nov, 2, -480 };   // 8 hour offset
-Timezone usPT(bcPDT, bcPDT);
+//BC Canada Pacific Standard Time always
+TimeChangeRule bcPDT = { "PDT", Second, dowSunday, Mar, 2, -420 };  // 7 hour offset - BC, Canada
+
+//Edit the info in the (xxx,xxx) according To Your Timezone and Daylight Saving Time
+// for example if in the US pacific northwest you'd use
+// Timezone timeZoneRule(usPDT, usPST);
+// Examples are provided above for various timezone settings
+//
+Timezone timeZoneRule(bcPDT, bcPDT);
+
 //Pointer To The Time Change Rule, Use to Get The TZ Abbrev
 TimeChangeRule *tcr;
 time_t utc;
